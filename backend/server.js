e815ce2b08a8652492b9fdf4390c52b649ecd413
Awanish Mani Tripathi <awanishmani2615@gmail.com>
2026-05-31 15:58:55 +0530

    Changes

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,26 @@ connectDB();
 const app = express();
 
 // Middleware
+// app.use(
+//   cors({
+//     origin: process.env.CLIENT_URL || "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
+const allowedOrigins = [
+  "http://localhost:5173",
+  process.env.CLIENT_URL,
+];
+
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
